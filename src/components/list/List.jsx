3d126eb224e353ac08355b38f9a04ed26e3b108c
hajerafatima <hajeerafatima6786@gmail.com>
2025-03-18 02:16:13 +0530

    shoppinf-application

---
+++ src/components/list/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./List.css";
+
+function List({ products }) {
+  return (
+    <ol>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ol>
+  );
+}
+export default List;
